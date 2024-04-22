19e325738bd987e793fa7fc73deca01e171d360e
Camila <cami.franco.etura08@gmail.com>
2024-04-22 20:21:20 -0300

    Desafio1

---
+++ Desafio1/main.js
@@ -0,0 +1,9 @@
+let nombreProducto = alert(" Azucar");
+
+let precioUnidad = 100
+
+let cantidad = prompt("Agregue la cantidad deseada:");
+
+console.log(cantidad * precioUnidad);
+
+alert( "El costo de su compra es de: " + cantidad * precioUnidad);
