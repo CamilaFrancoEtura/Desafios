4c045a95ee913699a09955f2b59c0bd6c478f7e2
CamilaFrancoEtura <167319106+CamilaFrancoEtura@users.noreply.github.com>
2024-05-02 19:34:23 -0300

    Add files via upload

---
+++ Desafio2/main.js
@@ -0,0 +1,9 @@
+let nombreProducto = alert(" Leche"); 
+let precioUnidad = 100
+let cantidad = parseInt(prompt("Ingrese la cantidad deseada del producto:"));
+let costoTotal ;
+
+if(cantidad < 5){alert(cantidad * precioUnidad);}
+
+else{alert( costoTotal = cantidad * precioUnidad * 0.9);
+}
