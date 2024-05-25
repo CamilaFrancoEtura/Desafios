681dc5faf52bfec90235bedfbd39529565ebf004
CamilaFrancoEtura <167319106+CamilaFrancoEtura@users.noreply.github.com>
2024-05-24 22:33:07 -0300

    Add files via upload

---
+++ Desafio6/main.js
@@ -0,0 +1,14 @@
+let Producto = document.getElementById("nombreProducto");
+console.log(Producto)
+
+let precio = document.getElementById("precioProducto");
+console.log(precioProducto)
+
+
+const titulo = document.querySelector("h1");
+titulo.style.color = "purple";
+titulo.style.fontSice = "55px";
+titulo.style.textAlign = "center";
+
+const parrafo = document.querySelector("p");
+parrafo.innerText = "Precio $450"
